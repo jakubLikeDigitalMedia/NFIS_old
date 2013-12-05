71b3f2257c171bb8f97cb6d260c09ae8139fd0fe
jacob <jakub.adler@likedigitalmedia.com>
2013-12-05 18:42:00 +0000

    alex started on registration form

---
+++ public/pages/registration.php
@@ -0,0 +1,58 @@
+<!-- REGISTRATION PAGE -->
+
+<head>
+<!-- Base CSS -->
+<link rel="stylesheet" type="text/css" href="../css/base.css"> 
+<script type="text/javascript" src="../js/modernizr.js"></script>
+</head>
+
+<body>
+
+<form action="demo_form.asp" method="get">
+  
+  <label for="fname">First name: </label> <input type="text" name="fname"><br/>
+  <label for="lname">First name: </label><input type="text" name="lname"><br/>
+  <label for="dob">Date of Birth: </label><input type="text" name="dob"><br/>
+  <label for="dob">Email: </label><input type="text" name="email"><br/>
+  <label for="phone">Phone: </label><input type="text" name="phone"><br/>
+  <label for="job">Job Title: </label>
+  
+  <select>
+  	<option value="job1">Job No 1</option>
+  	<option value="job2">Job No 2</option>
+  	<option value="job3">Job No 3</option>
+  	<option value="job4">Job No 4</option>
+  </select><br/>
+  
+  <label for="brand">Brand: </label>
+  <select>
+  	<option value="brand1">Brand 1</option>
+  	<option value="brand2">Brand 2</option>
+  	<option value="brand3">Brand 3</option>
+  	<option value="brand4">Brand 4</option>
+  </select><br/>
+  
+  <label for="department">Department: </label>
+  <select>
+  	<option value="department1">Department 1</option>
+  	<option value="department2">Department 2</option>
+  	<option value="department3">Department 3</option>
+  	<option value="department4">Department 4</option>
+  </select><br/>
+  
+  <label for="superior">Superior </label>
+  <select>
+  	<option value="superior1">Superior 1</option>
+  	<option value="superior2">Superior 2</option>
+  	<option value="superior3">Superior 3</option>
+  	<option value="superior4">Superior 4</option>
+  </select><br/>
+  <label for="previousjob">Previous Job: </label>  
+  <button type="button">Add +</button><br/>
+  
+    
+  <input type="submit" value="Submit">
+</form>	
+	
+	
+</body>
